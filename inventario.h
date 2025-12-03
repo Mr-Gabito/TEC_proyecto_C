3c91bf5bae2978fc4c197192540df84dfe63fc21
Mr-Gabito <a01709333@tec.mx>
2025-12-03 10:04:59 -0600

    updatemain

--- inventario.h
+++ inventario.h
@@ -1,3 +1,4 @@
+
 #ifndef INVENTARIO_H
 #define INVENTARIO_H
 #include "libro.h"
@@ -29,17 +30,17 @@ Inventory::Inventory(){
 }
 
 void Inventory::createSoftlist(){
-    bookS[0] = Softcover(ibookS, "The Little Prince", 96);
+    bookS[0] = Softcover(ibookS + 1, "The Little Prince", 96);
     ibookS += 1;
-    bookS[1] = Softcover(ibookS, "Eragon", 503);
+    bookS[1] = Softcover(ibookS + 1, "Eragon", 503);
     ibookS += 1;
-    bookS[2] = Softcover(ibookS, "The Lord of the Flies", 304);
+    bookS[2] = Softcover(ibookS + 1, "The Lord of the Flies", 304);
     ibookS += 1;
-    bookS[3] = Softcover(ibookS, "The Underneath", 320);
+    bookS[3] = Softcover(ibookS + 1, "The Underneath", 320);
     ibookS += 1;
-    bookS[4] = Softcover(ibookS, "Miss Peregrine's Home for Peculiar Children", 352);
+    bookS[4] = Softcover(ibookS + 1, "Miss Peregrine's Home for Peculiar Children", 352);
     ibookS += 1;
-    bookS[5] = Softcover(ibookS, "The Giver", 208);
+    bookS[5] = Softcover(ibookS + 1, "The Giver", 208);
     ibookS += 1;
 };
 
@@ -74,36 +75,33 @@ void Inventory::createDigitallist(){
 };
 
 void Inventory::showSoft(){
-    for(int i = 0; i <= ibookS; i++){
+    for(int i = 0; i < ibookS; i++){
         std::cout << "ID: " << bookS[i].getID() << std::endl;
         std::cout << "Title: " << bookS[i].get_title() << std::endl;
-        std::cout << "Number of pages: " << bookS[i].get_num_pag() << std::endl;
-        std::cout << "Price: " << bookS[i].calc_price(0, bookS[i].get_num_pag(), false) << std::endl << std::endl;
+        std::cout << "Number of pages: " << bookS[i].get_num_pag() << std::endl << std::endl;
     }
 }
 
 void Inventory::showHard(){
-    for(int i = 0; i <= ibookH; i++){
+    for(int i = 0; i < ibookH; i++){
         std::cout << "ID: " << bookH[i].getID() << std::endl;
         std::cout << "Title: " << bookH[i].get_title() << std::endl;
-        std::cout << "Number of pages: " << bookH[i].get_num_pag() << std::endl;
-        std::cout << "Price: " << bookH[i].calc_price(0, bookH[i].get_num_pag(), false) << std::endl << std::endl;
+        std::cout << "Number of pages: " << bookH[i].get_num_pag() << std::endl << std::endl;
     }
 }
 
 void Inventory::showDigital(){
-    for(int i = 0; i <= ibookD; i++){
+    for(int i = 0; i < ibookD; i++){
         std::cout << "ID: " << bookD[i].getID() << std::endl;
         std::cout << "Title: " << bookD[i].get_title() << std::endl;
-        std::cout << "Number of pages: " << bookD[i].get_num_pag() << std::endl;
-        std::cout << "Price: " << bookD[i].calc_price(0, bookD[i].get_num_pag(), false) << std::endl << std::endl;
+        std::cout << "Number of pages: " << bookD[i].get_num_pag() << std::endl << std::endl;
     }
 }
 
 void Inventory::addBook(int option, std::string title, int num_pag){
     if(option == 1){
         ibookS += 1;
-        Softcover aux(ibookS, title, num_pag);
+        Softcover aux(ibookS + 1, title, num_pag);
         bookS[ibookS] = aux;
     }
     else if(option == 2){
--- main.cpp
+++ main.cpp
@@ -19,30 +19,35 @@ int main(){
     inventory.createSoftlist();
     inventory.createHardlist();
     inventory.createDigitallist();
+    Softcover softcover;
+    Hardcover hardcover;
+    Digital digital;
     
     std::string nameb;
     int num_pages = 0;
     int option = 0;
     int optionn = 0;
+    int optionnn = 0;
+    std::string tof;
 
     while(option != 6){
         menu();
         std::cin >> option;
         switch(option){
-            case 1:
+        case 1:
             inventory.showSoft();
             break;
-            case 2:
+        case 2:
             inventory.showHard();
             break;
-            case 3:
+        case 3:
             inventory.showDigital();
             break;
-            case 4:
+        case 4:
             while (optionn != 4){
-                std::cout << "1. Softcover book:" << std::endl;
-                std::cout << "2. Hardcover book:" << std::endl;
-                std::cout << "3. Digital book:" << std::endl;
+                std::cout << "1. Softcover book" << std::endl;
+                std::cout << "2. Hardcover book" << std::endl;
+                std::cout << "3. Digital book" << std::endl;
                 std::cout << "4. Back" << std::endl;
                 std::cin >> optionn;
                 if(optionn == 4){
@@ -54,9 +59,60 @@ int main(){
                 std::cin >> num_pages;
                 inventory.addBook(optionn, nameb, num_pages);
             }
-            //case 5:
-            //take price off the lists that show and try to see if you can calculate the price correctly and as you intended
+            break;
+        case 5:
+        //take price off the lists that show and try to see if you can calculate the price correctly and as you intended
+            while (optionnn != 4){
+                std::cout << "1. Softcover book" << std::endl;
+                std::cout << "2. Hardcover book" << std::endl;
+                std::cout << "3. Digital book" << std::endl;
+                std::cout << "4. Back" << std::endl;
+                std::cin >> optionnn;
+                if(optionnn == 1){
+                    inventory.showSoft();
+                    std::cout << "Type the page number of pages of the book you'd like to know the price of: " << std::endl;
+                    std::cin >> num_pages;
+                    std::cout << "Would you like your book to be special edition? (Yes or No): " << std::endl;
+                    std::cin >> tof;
+                    if (tof == "Yes"){
+                        softcover.calc_price(0, num_pages, true);
+                    }
+                    else{
+                        softcover.calc_price(0, num_pages, false);
+                    }
+                }
+                else if(optionnn == 2){
+                    inventory.showHard();
+                    std::cout << "Type the page number of pages of the book you'd like to know the price of: " << std::endl;
+                    std::cin >> num_pages;
+                    std::cout << "Would you like your book to be special edition? (Yes or No): " << std::endl;
+                    std::cin >> tof;
+                    if (tof == "Yes"){
+                        hardcover.calc_price(0, num_pages, true);
+                    }
+                    else{
+                        hardcover.calc_price(0, num_pages, false);
+                    }
+                }
+                else if(optionnn == 3){
+                    inventory.showDigital();
+                    std::cout << "Type the page number of pages of the book you'd like to know the price of: " << std::endl;
+                    std::cin >> num_pages;
+                    std::cout << "Would you like your book to be an audiobook? (Yes or No): " << std::endl;
+                    std::cin >> tof;
+                    if(tof == "Yes"){
+                        digital.calc_price(0, num_pages, true);
+                    }
+                    else{
+                        digital.calc_price(0, num_pages, false);
+                    }
+                }
+                else if(optionnn == 4){
+                    break;
+                }
+            }
             break;
         }
     }
-};
+    return 0;
+}
